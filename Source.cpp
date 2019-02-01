a23217345d3b843966266f225d3e0ed6710c1417
Keith Brockunier <brocku15@rowan.edu>
2019-01-31 19:22:13 -0500

    Added code for circumference and miles conversions

--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,6 @@
-
-
-
-/***********************************************
-* Name: Keith Brockunier
-* Email: brocku15@students.rowan.edu
-***********************************************
-* Purpose:
-*  This program calculates area of circle
-***********************************************/
-
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 const  float PI = 3.14159;
 
@@ -19,24 +8,49 @@ int main()
 {
 
 	float radius, area, circumference;
-	
+
 	//Display purpose of the program
-	cout << " ******************************************"<<endl;
-	cout << " This program takes in radius to compute area of circle" <<endl;
+	cout << " ******************************************" << endl;
+	cout << " This program takes in radius to compute circumference of circle" << endl;
 	cout << " ******************************************\n\n";
-	
+
 	//Get user input
 	cout << "Please input radius: ";
 	cin >> radius;
-	
-	//Compute area to PI*radius*radius
-	area = PI* radius *radius; 
-	//ghh
-	//Display the area 
-	cout << " The area is = " << area << "\n\n";
-	cout << "Program has ended"<<endl;
+
+	//Compute circumference 2PI*R
+	circumference = PI * radius * 2;
+
+	//Display the circumference
+	cout << " The circumference of the circle is = " << circumference << "\n\n";
+	cout << "Program has ended" << endl;
+
+	float mile, kilometer, meter, centimeter, milimeter;
+
+	//Display purpose of the program
+	cout << " ******************************************" << endl;
+	cout << " This program converts miles into kilometers, meters, centimeters, and milimeters" << endl;
+	cout << " ******************************************\n\n";
+
+	//Get user input
+	cout << "Please input miles: ";
+	cin >> mile;
+
+	//Compute the conversions 
+	kilometer = mile * 1.6;
+	meter = kilometer * 1000;
+	centimeter = meter * 100;
+	milimeter = centimeter * 10;
+
+	//Display the conversions 
+	cout << " The conversion of kilometer from miles is = " << kilometer << "\n\n";
+	cout << " The conversion of meter from miles is = " << meter << "\n\n";
+	cout << " The conversion of centimeter from miles is = " << centimeter << "\n\n";
+	cout << " The conversion of milimeter from miles is = " << milimeter << "\n\n";
+	cout << "Program has ended" << endl;
+
 
 	system("pause");
-	
+
 	return 0;
 }
